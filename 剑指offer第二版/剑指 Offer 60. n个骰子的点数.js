92e50faa96b24f8f81dc4678c6ae6027f2373211
ghter123 <ghter123@hotmail.com>
2020-06-23 17:41:47 +0800

    add

--- 剑指offer第二版/剑指 Offer 60. n个骰子的点数.js
+++ 剑指offer第二版/剑指 Offer 60. n个骰子的点数.js
@@ -2,6 +2,21 @@
  * @param {number} n
  * @return {number[]}
  */
-var twoSum = function(n) {
-
+var twoSum = function (n) {
+    const result = [
+        [1 / 6, 1 / 6, 1 / 6, 1 / 6, 1 / 6, 1 / 6]
+    ]
+    for (i = 1; i < n; i++) {
+        result.push([])
+        for (j = i - 1; j < result[i - 1].length; j++) {
+            for (m = 1; m <= 6; m++) {
+                if (!result[i][j + m]) {
+                    result[i][j + m] = result[i - 1][j] * 1 / 6
+                } else {
+                    result[i][j + m] += result[i - 1][j] * 1 / 6
+                }
+            }
+        }
+    }
+    return result[n - 1].slice(n - 1, n * 6)
 };
