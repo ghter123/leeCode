530024f3b95dca4fed592feec47098958ba156c1
LiuQin <ghter123@hotmail.com>
2020-06-23 22:37:50 +0800

    add

--- 剑指offer第二版/剑指 Offer 66. 构建乘积数组.js
+++ 剑指offer第二版/剑指 Offer 66. 构建乘积数组.js
@@ -3,5 +3,15 @@
  * @return {number[]}
  */
 var constructArr = function (a) {
-    
+    if (a.length === 0) return [0]
+    const b = [1]
+    let tmp = 1
+    for (let i = 1; i < a.length; i++) {
+        b[i] = b[i - 1] * a[i - 1];
+    }
+    for (let i = a.length - 2; i >= 0; i--) {
+        tmp *= a[i + 1];
+        b[i] *= tmp;
+    }
+    return b
 };
