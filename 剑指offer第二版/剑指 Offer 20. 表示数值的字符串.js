3898ba3ce3842f46d535747e57067a7bb78af922
LiuQin <ghter123@hotmail.com>
2020-06-25 22:17:20 +0800

    add

--- 剑指offer第二版/剑指 Offer 20. 表示数值的字符串.js
+++ 剑指offer第二版/剑指 Offer 20. 表示数值的字符串.js
@@ -2,27 +2,59 @@
  * @param {string} s
  * @return {boolean}
  */
+
+/**
+ * states
+ * 0 开始
+ * 1 sign
+ * 2 .
+ * 3 digit
+ * 4 digitAfterDot
+ * 5 e
+ * 6 signAfterE
+ * 7 digitAfterE
+ * 8 dotAfterDigit
+ * @param {*} s 
+ */
 var isNumber = function (s) {
     s = s.trim()
-    const isNum = (num) => { return /^[0-9]+$/.test(num) }
-    if (s[0] === '+' || s[0] === '-' || s[0] === '.') {
-        if (!isNum(s[1])) return false
-    } else {
-        if (!isNum(s[0])) return false
+    const states = []
+    states.push(new Map([['sign', 1], ['.', 2], ['digit', 3]]))         // 0
+    states.push(new Map([['digit', 3], ['.', 2]]))                      // 1
+    states.push(new Map([['digit', 4]]))                                // 2
+    states.push(new Map([['digit', 3], ['e', 5], ['.', 8]]))            // 3 
+    states.push(new Map([['e', 5], ['digit', 4]]))                      // 4
+    states.push(new Map([['sign', 6], ['digit', 7]]))                   // 5
+    states.push(new Map([['digit', 7]]))                                // 6
+    states.push(new Map([['digit', 7]]))                                // 7
+    states.push(new Map([['digit', 4], ['e', 5]]))                      // 8
+    let p = 0
+    let t;
+    for (const c of s) {
+        if (c >= '0' && c <= '9') t = 'digit'
+        else if (c === '+' || c === '-') t = 'sign'
+        else t = c
+        if (!states[p].has(t)) return false
+        p = states[p].get(t)
     }
-    for (i = 1; i < s.length - 1; i++) {
-        const next = s[i + 1]
-        const cur = s[i]
-          
-            if (isNum(cur)) continue
-            else if (cur === '.') {
-                if (!isNum(s.slice(i + 1) || 0)) return false
-            }
-            else if (cur === 'e') {
-                if ((!isNum(next) || next === '0') || !isNum(s.slice(i + 1) || 0)) return false
-            }
-            else return false
-        
-    }
-    return true
+    return p === 3 || p === 4 || p === 7 || p === 8
 };
+
+const assert = require('assert')
+
+assert.equal(isNumber("+100"), true)
+assert.equal(isNumber("5e2"), true)
+assert.equal(isNumber("-123"), true)
+assert.equal(isNumber("3.1416"), true)
+assert.equal(isNumber("0123"), true)
+assert.equal(isNumber("3."), true)
+assert.equal(isNumber("+.8"), true)
+assert.equal(isNumber("46.e3"), true)
+assert.equal(isNumber("12e"), false)
+assert.equal(isNumber("1a3.14"), false)
+assert.equal(isNumber("1.2.3"), false)
+assert.equal(isNumber("+-5"), false)
+assert.equal(isNumber("-1E-16"), false)
+assert.equal(isNumber("12e+5.4"), false)
+assert.equal(isNumber("."), false)
+assert.equal(isNumber(" -."), false)
