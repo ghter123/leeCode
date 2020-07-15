34ee802017fd4b5161cb38a7b656eda6f75f9810
ghter123 <ghter123@hotmail.com>
2020-07-15 17:36:41 +0800

    update

--- 274. H 指数.js
+++ 274. H 指数.js
@@ -6,12 +6,7 @@ var hIndex = function (citations) {
     citations = citations.sort((a, b) => a - b)
     let maxH = 0
     for (let i = 0; i < citations.length; i++) {
-        let temp = 0
-        if (citations[i] <= citations.length - i) {
-            temp = citations[i]
-        } else {
-            temp = citations.length - i
-        }
+        const temp = citations[i] <= citations.length - i ? citations[i] : citations.length - i
         if (maxH < temp) maxH = temp
     }
     return maxH
